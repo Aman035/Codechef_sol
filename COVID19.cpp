efb11a708c0b60b9aecacc7c8ab540a00fe08b53
codebreakeragr8 <54989169+codebreakeragr8@users.noreply.github.com>
2020-05-16 17:04:40 +0530

    Update COVID19.cpp

--- COVID19.cpp
+++ COVID19.cpp
@@ -1,42 +1,44 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int t,n,i,j,b,x;
-	cin>>t;
 	
-	for(i=0;i<t;i++){
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long int n;
 	    cin>>n;
-	    int a[n];
-	    vector<int> v;
+	    long int A[n];
+	    long int infectmin=INT_MAX;
+	    long int infectmax=1;
+	    long int k=1;
 	    
-	    for(j=0;j<n;j++)
-	    cin>>a[j];
+	    for(long int i=0;i<n;i++)
+	    cin>>A[i];
 	    
-	    x=1;
-	    
-	    for(j=1;j<n;j++){
-	        if(a[j]-a[j-1]>2){
-	            v.push_back(x);
-	            x=1;
+	    for(long int i=0;i<n-1;i++)
+	    {
+	        if(A[i+1]-A[i]<=2)
+	            k++;
+	        else
+	        {
+	            if(infectmin>k)
+	            infectmin=k;
+	            if(infectmax<k)
+	            infectmax=k;
+	            k=1;
 	        }
-	        else{
-	            x++;
-	            if(j==n-1)
-	            v.push_back(x);
-	            
-	        }
-	        if(a[n-1]-a[n-2]>2)
-	        v.push_back(1);
-	        
 	    }
-	    sort(v.begin(),v.end());
-	    int c = v.size();
-	    cout<<v[0]<<" "<<v[c-1]<<endl;
-	   
+	    
+	         if(infectmin>k)
+	            infectmin=k;
+	            
+            if(infectmax<k)
+	            infectmax=k;
+	    
+	    cout<<infectmin<<" "<<infectmax<<"\n";
 	    
 	}
-	
-	
 	return 0;
 }
